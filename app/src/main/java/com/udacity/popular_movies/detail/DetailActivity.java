a667e18d3061ce1bc5aa3d9ee448856e71e58ed1
Torben Möller <torben.moeller@gmx.net>
2018-08-01 19:31:53 +0200

    Trailer and Reviews added

--- app/src/main/java/com/udacity/popular_movies/detail/DetailActivity.java
+++ app/src/main/java/com/udacity/popular_movies/detail/DetailActivity.java
@@ -2,27 +2,37 @@ package com.udacity.popular_movies.detail;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Movie;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.popular_movies.BuildConfig;
 import com.udacity.popular_movies.Config;
+import com.udacity.popular_movies.MovieViewHolder;
 import com.udacity.popular_movies.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.MovieDb;
+import info.movito.themoviedbapi.model.Reviews;
 import info.movito.themoviedbapi.model.Video;
 
 public class DetailActivity extends AppCompatActivity {
@@ -37,10 +47,14 @@ public class DetailActivity extends AppCompatActivity {
     TextView tv_release_date;
     @BindView(R.id.tv_user_rating)
     TextView tv_user_rating;
-    @BindView(R.id.btn_watch_trailer)
-    Button btn_watch_trailer;
-
-    String trailerKey;
+    @BindView(R.id.list_trailers)
+    LinearLayout list_trailers;
+    @BindString(R.string.trailer_label)
+    String trailer;
+    @BindView(R.id.list_reviews)
+    LinearLayout list_reviews;
+    @BindString(R.string.review_by)
+    String review_by;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,18 +82,22 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, "Error while loading movie data", Toast.LENGTH_SHORT).show();
     }
 
-    private void fillActivity(MovieDb movieDb, String completePath) {
+    private void fillActivity(MovieContainer container, String completePath) {
         try {
             Picasso.with(getApplicationContext())
                     .load(completePath)
                     .into(imageView);
+            MovieDb movieDb = container.getMovieDb();
+            List<String> trailers = container.getTrailers();
             tv_title.setText(movieDb.getTitle());
             tv_synopsis.setText(movieDb.getOverview());
             tv_release_date.setText(movieDb.getReleaseDate());
             tv_user_rating.setText(String.valueOf(movieDb.getVoteAverage()));
-            trailerKey = getTrailer(movieDb);
-            if(trailerKey == null){
-                btn_watch_trailer.setText("Not available");
+            for (int i = 0; i < trailers.size(); i++){
+                createTrailerButton(trailers.get(i), i + 1);
+            }
+            for (Reviews review : movieDb.getReviews()){
+                createReview(review);
             }
         } catch (Exception e) {
             Log.e("DetailActivity", e.getMessage());
@@ -87,52 +105,80 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    @OnClick(R.id.btn_watch_trailer)
-    public void openYoutube(Button button) {
-        if(trailerKey == null){
-            return;
-        }
-//        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id));
-        Intent webIntent = new Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("http://youtu.be/" + trailerKey));
-//        try {
-            getApplicationContext().startActivity(webIntent);
-//        } catch (ActivityNotFoundException ex) {
-//            context.startActivity(webIntent);
-//        }
-
-
-    }
-
-
-
-    private String getTrailer(MovieDb movieDb){
-        for(Video v : movieDb.getVideos()){
-            if(v.getType().equals("Trailer") && v.getSite().equals("YouTube")){
-                return v.getKey();
+    private void createTrailerButton(final String trailerKey, int number){
+        Button watchTrailerButton = new Button(this);
+        watchTrailerButton.setText(trailer + " " + number);
+        list_trailers.addView(watchTrailerButton);
+        watchTrailerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent webIntent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://youtu.be/" + trailerKey));
+                getApplicationContext().startActivity(webIntent);
             }
-        }
-        return null;
+        });
+    }
+
+    private void createReview(Reviews review){
+        TextView authorView = new TextView(this);
+        authorView.setTypeface(null, Typeface.BOLD);
+        authorView.setText(review_by + " " +review.getAuthor());
+        TextView contentView = new TextView(this);
+        contentView.setText(review.getContent());
+        list_reviews.addView(authorView);
+        list_reviews.addView(contentView);
     }
 
 
-
-    class MovieTask extends AsyncTask<Integer, Void, MovieDb> {
+    class MovieTask extends AsyncTask<Integer, Void, MovieContainer> {
 
         TmdbApi api;
 
         @Override
-        protected MovieDb doInBackground(Integer... ids) {
+        protected MovieContainer doInBackground(Integer... ids) {
             int id = ids[0];
             api = new TmdbApi(BuildConfig.TMDB_API_KEY);
-            return api.getMovies().getMovie(id, Config.getLanguage(), TmdbMovies.MovieMethod.videos);
+            MovieDb movieDb = api.getMovies().getMovie(id, Config.getLanguage(), TmdbMovies.MovieMethod.videos, TmdbMovies.MovieMethod.reviews);
+            List<String> trailers = new ArrayList<>();
+            for (Video v : movieDb.getVideos()) {
+                if (v.getType().equals("Trailer") && v.getSite().equals("YouTube")) {
+                    trailers.add(v.getKey());
+                }
+            }
+            return new MovieContainer(movieDb, trailers);
         }
 
         @Override
-        protected void onPostExecute(MovieDb result) {
-            String completePath = api.getConfiguration().getBaseUrl() + Config.getHighResolution() + result.getBackdropPath();
+        protected void onPostExecute(MovieContainer result) {
+            String completePath = api.getConfiguration().getBaseUrl() + Config.getHighResolution() + result.getMovieDb().getBackdropPath();
             fillActivity(result, completePath);
+        }
+    }
+
+    class MovieContainer {
+        MovieDb movieDb;
+        List<String> trailers;
+
+        public MovieContainer(MovieDb movieDb, List<String> trailers) {
+            this.movieDb = movieDb;
+            this.trailers = trailers;
+        }
+
+        public MovieDb getMovieDb() {
+            return movieDb;
+        }
+
+        public void setMovieDb(MovieDb movieDb) {
+            this.movieDb = movieDb;
+        }
+
+        public List<String> getTrailers() {
+            return trailers;
+        }
+
+        public void setTrailers(List<String> trailers) {
+            this.trailers = trailers;
         }
     }
 }
